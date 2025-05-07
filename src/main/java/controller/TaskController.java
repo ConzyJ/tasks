6411b466b221a6dceb9866930bf711bcef8b846a
ConzyJ <krzys.osmalek@gmail.com>
2025-05-07 23:01:57 +0200

    Implementation 18.2 - JPA, connection to MySQL, service, mapper, repository added

--- src/main/java/controller/TaskController.java
+++ src/main/java/controller/TaskController.java
@@ -1,37 +1,47 @@
 package controller;
 
 import com.crud.tasks.domain.TaskDto;
+import mapper.TaskMapper;
+import service.DbService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
-import java.util.ArrayList;
 import java.util.List;
 
 @RestController
 @RequestMapping("/v1/tasks")
+@RequiredArgsConstructor
 public class TaskController {
+
+    private final DbService service;
+    private final TaskMapper taskMapper;
 
     @GetMapping
     public List<TaskDto> getTasks() {
-        return new ArrayList<>();
+        return taskMapper.mapToTaskDtoList(service.getAllTasks());
     }
 
     @GetMapping("/{taskId}")
-    public TaskDto getTask(@PathVariable Long taskId) {
-        return new TaskDto(1L, "test title", "test_content");
+    public ResponseEntity<TaskDto> getTask(@PathVariable Long taskId) {
+        return service.getTaskById(taskId)
+                .map(taskMapper::mapToTaskDto)
+                .map(ResponseEntity::ok)
+                .orElse(ResponseEntity.notFound().build());
     }
 
-    @DeleteMapping("/{taskId}")
-    public void deleteTask(@PathVariable Long taskId) {
-
+    @PostMapping
+    public TaskDto createTask(@RequestBody TaskDto taskDto) {
+        return taskMapper.mapToTaskDto(service.saveTask(taskMapper.mapToTask(taskDto)));
     }
 
     @PutMapping
     public TaskDto updateTask(@RequestBody TaskDto taskDto) {
-        return new TaskDto(1L, "Edited test title", "Test content");
+        return taskMapper.mapToTaskDto(service.saveTask(taskMapper.mapToTask(taskDto)));
     }
 
-    @PostMapping
-    public void createTask(@RequestBody TaskDto taskDto) {
-
+    @DeleteMapping("/{taskId}")
+    public void deleteTask(@PathVariable Long taskId) {
+        service.deleteTask(taskId);
     }
 }
